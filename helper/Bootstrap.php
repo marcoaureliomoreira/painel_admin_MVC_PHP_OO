2a561d1539487b9beec32a450de90e481635b1b8
Marcoaurélio <marcomoreiracardoso@gmail.com>
2019-01-02 23:11:10 -0200

    First Commit - System, Index and Bootstrap

---
+++ helper/Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+spl_autoload_register(function($class)
+{
+    $file = str_repeat('\\','/',$class);
+
+    if (file_exists($file))
+    {
+        require_once $file;
+    }
+});
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'helper/Bootstrap.php';
+
+use lib\System;
+
+$System = new System();
+$System->Run();
